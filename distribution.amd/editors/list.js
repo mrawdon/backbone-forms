0f5df351fcd3e1bbf130abde587cf329fd9aa90a
mike <mike@mitrend.com>
2014-01-20 21:32:43 -0500

    Built

--- distribution.amd/editors/list.js
+++ distribution.amd/editors/list.js
@@ -384,7 +384,7 @@ define(['jquery', 'underscore', 'backbone', 'backbone-forms'], function($, _, Ba
 
     //STATICS
     template: _.template('\
-      <div>\
+      <div class="listEntry">\
         <span data-editor></span>\
         <button type="button" data-action="remove">&times;</button>\
       </div>\
